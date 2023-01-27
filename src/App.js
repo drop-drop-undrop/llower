aea7cc80181246f733555ffe95804b9166e77a00
Muhammad Iqbal Zulfikar <cauliframes@gmail.com>
2023-01-27 09:33:19 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,374 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import Loading from './Loading'
+import { Twitter, useTwitter } from './Twitter'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Twitter>
+      <Loading>
+        <Title />
+        <Description />
+      </Loading>
+    </Twitter>
+  )
 }
 
-export default App;
+function Title() {
+  return (
+    <Heading style={{
+      borderWidth: 2,
+      borderColor: 'black',
+      borderStyle: 'solid',
+      borderRadius: '9px',
+      boxShadow: '0.5px 3px 1px',
+    }}
+    >
+      <div style={{
+        borderWidth: 2,
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderRadius: '9px',
+        padding: 7,
+      }}
+      >
+        llower.
+      </div>
+    </Heading>
+  )
+}
+
+const Heading = styled.div(({ z }) => ({
+  display: 'inline-block',
+  position: 'absolute',
+  left: 19,
+  top: 19,
+  fontSize: '2em',
+  fontWeight: 'bolder',
+  padding: 9,
+  margin: 0,
+  zIndex: z || 'auto',
+  textAlign: 'center',
+}))
+
+function Description() {
+  const twitter = useTwitter()
+  const [status, setStatus] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    twitter.subscribeToTokens(setStatus)
+    twitter.subscribeToUser(setUser)
+    return () => {
+      twitter.unsubscribeFromUser(setUser)
+      twitter.unsubscribeFromTokens(setStatus)
+    }
+  }, [twitter])
+
+  if (!status) {
+    return (
+      <Center>
+        <Column>
+          <div style={{
+            fontSize: '4em',
+            fontWeight: 'bold',
+            position: 'relative',
+            left: -29,
+            borderWidth: 2,
+            borderColor: 'black',
+            borderStyle: 'solid',
+            borderRadius: '9px',
+            boxShadow: '0.5px 3px 1px',
+            padding: 9,
+          }}
+          >
+            <div style={{
+              borderWidth: 2,
+              borderColor: 'black',
+              borderStyle: 'solid',
+              borderRadius: '9px',
+              boxShadow: '0.5px 3px 1px',
+              padding: 9,
+            }}
+            >
+              <div style={{
+                borderWidth: 2,
+                borderColor: 'black',
+                borderStyle: 'solid',
+                borderRadius: '9px',
+                padding: 9,
+              }}
+              >
+                Hello.
+              </div>
+            </div>
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            You are not
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            logged in yet,
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            you can &nbsp;
+            <LoginButton />
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            with Twitter 🐦;
+          </div>
+        </Column>
+      </Center>
+    )
+  } else {
+    return (
+      <Center>
+        <Column>
+          <div style={{
+            fontSize: '4em',
+            fontWeight: 'bold',
+            position: 'relative',
+            left: -29,
+            borderWidth: 2,
+            borderColor: 'black',
+            borderStyle: 'solid',
+            borderRadius: '9px',
+            boxShadow: '0.5px 3px 1px',
+            padding: 9,
+          }}
+          >
+            <div style={{
+              borderWidth: 2,
+              borderColor: 'black',
+              borderStyle: 'solid',
+              borderRadius: '9px',
+              boxShadow: '0.5px 3px 1px',
+              padding: 9,
+            }}
+            >
+              <div style={{
+                borderWidth: 2,
+                borderColor: 'black',
+                borderStyle: 'solid',
+                borderRadius: '9px',
+                padding: 9,
+              }}
+              >
+                Hi.
+              </div>
+            </div>
+          </div>
+          {user && (
+            <div style={{
+              fontSize: '2em',
+              fontWeight: 'bold',
+              position: 'relative',
+              left: -33,
+            }}
+            >
+              <a
+                href={`https://twitter.com/${user.username}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @
+                {user.username}
+              </a>
+            </div>
+          )}
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            You are now
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            logged in 👏,
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            you can &nbsp;
+            <FollowButton />
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            🧑‍🚀 &nbsp;
+            <a
+              href="https://twitter.com/nasa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @NASA
+            </a>
+          </div>
+          <div style={{
+            fontSize: '2em',
+            fontWeight: 'bold',
+          }}
+          >
+            or &nbsp;
+            <LogoutButton />
+            &nbsp; ;
+          </div>
+        </Column>
+      </Center>
+    )
+  }
+}
+
+const Center = styled.div({
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const Column = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+function LoginButton({ style }) {
+  const twitter = useTwitter()
+
+  return (
+    <Button
+      type="button"
+      onClick={twitter.requestAuthorization}
+      style={style || {
+        fontSize: '0.9em',
+        fontWeight: 'bold',
+      }}
+    >
+      log in
+    </Button>
+  )
+}
+
+function FollowButton({ style }) {
+  const twitter = useTwitter()
+  const [following, setFollowing] = useState(null)
+
+  useEffect(() => {
+    twitter.subscribeToFollowing(setFollowing)
+    return () => {
+      twitter.unsubscribeFromFollowing(setFollowing)
+    }
+  }, [twitter])
+
+  if (!following) {
+    return (
+      <Button
+        type="button"
+        onClick={twitter.requestFollowingOfNasa}
+        style={style || {
+          fontSize: '0.9em',
+          fontWeight: 'bold',
+        }}
+      >
+        follow
+      </Button>
+    )
+  } else {
+    return (
+      <Button
+        type="button"
+        onClick={twitter.requestUnfollowingOfNasa}
+        style={style || {
+          fontSize: '0.9em',
+          fontWeight: 'bold',
+        }}
+      >
+        unfollow
+      </Button>
+    )
+  }
+}
+
+function LogoutButton({ style }) {
+  const twitter = useTwitter()
+
+  return (
+    <Button
+      type="button"
+      onClick={twitter.requestDeauthorization}
+      style={style || {
+        fontSize: '0.9em',
+        fontWeight: 'bold',
+      }}
+    >
+      log out
+    </Button>
+  )
+}
+
+const Button = styled.button({
+  alignItems: 'center',
+  appearance: 'none',
+  backgroundColor: '#FCFCFD',
+  borderRadius: '4px',
+  boxShadow: [
+    'rgba(45, 35, 66, 0.4) 0 2px 4px',
+    'rgba(45, 35, 66, 0.3) 0 7px 13px -3px',
+    '#D6D6E7 0 -3px 0 inset',
+  ].join(),
+  boxSizing: 'border-box',
+  color: '#36395A',
+  cursor: 'pointer',
+  fontFamily: 'monospace',
+  justifyContent: 'center',
+  listStyle: 'none',
+  paddingLeft: 11,
+  paddingRight: 11,
+  paddingBottom: 4,
+  textDecoration: 'none',
+  userSelect: 'none',
+  touchAction: 'manipulation',
+  willChange: [
+    'box-shadow',
+    'transform',
+  ].join(),
+  transition: [
+    'box-shadow .15s',
+    'transform .15s',
+  ].join(),
+  '&:hover': {
+    boxShadow: [
+      'rgba(45, 35, 66, 0.4) 0 4px 8px',
+      'rgba(45, 35, 66, 0.3) 0 7px 13px -3px',
+      '#D6D6E7 0 -3px 0 inset',
+    ].join(),
+    transform: 'translateY(-2px)',
+  },
+  '&:active': {
+    boxShadow: '#D6D6E7 0 3px 7px inset',
+    transform: 'translateY(2px)',
+  },
+})
---
+++ src/Loading.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import { useTwitter } from './Twitter'
+
+function Loading({ children }) {
+  const twitter = useTwitter()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    twitter.subscribeToLoading(setLoading)
+    return () => {
+      twitter.unsubscribeFromLoading(setLoading)
+    }
+  }, [twitter])
+
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          margin: 'auto',
+          background: '#f1f2f3',
+          display: 'block',
+          zIndex: '1',
+          position: 'absolute',
+          transitionDelay: '0.15s',
+          transition: 'visibility 0.7s, opacity 0.7s ease-in-out',
+          ...(loading
+            ? { visibility: 'visible', opacity: 1 }
+            : { visibility: 'hidden', opacity: 0 }),
+        }}
+        width="100%"
+        height="100%"
+        preserveAspectRatio="xMidYMid"
+      // viewBox="0 0 1084 322"
+      >
+        <defs>
+          <pattern id="pid-0.09336838170898965" x="0" y="0" width="140.8" height="140.8" patternUnits="userSpaceOnUse">
+            <g transform="scale(0.55)">
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#1d5167" stroke="#1d5167" strokeWidth="1" transform="translate(0 -51.2)">
+                <animateTransform attributeName="transform" type="translate" values="256 -51.2;0 -51.2;256 -51.2" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="0s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#437f79" stroke="#437f79" strokeWidth="1" transform="translate(0 0)">
+                <animateTransform attributeName="transform" type="translate" values="0 0;256 0;0 0" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="-0.6666666666666665s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#7aab92" stroke="#7aab92" strokeWidth="1" transform="translate(0 51.2)">
+                <animateTransform attributeName="transform" type="translate" values="256 51.2;0 51.2;256 51.2" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="-1.333333333333333s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#b0c7a2" stroke="#b0c7a2" strokeWidth="1" transform="translate(0 102.4)">
+                <animateTransform attributeName="transform" type="translate" values="0 102.4;256 102.4;0 102.4" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="-1.9999999999999998s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#e8f2d1" stroke="#e8f2d1" strokeWidth="1" transform="translate(0 153.6)">
+                <animateTransform attributeName="transform" type="translate" values="256 153.6;0 153.6;256 153.6" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="-2.666666666666666s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+              <path d="M -256 12.240000000000002 C -192 12.240000000000002 -192 2 -128 2 C -64 2 -64 12.240000000000002 2 12.240000000000002 C 64 12.240000000000002 64 2 128 2 C 192 2 192 12.240000000000002 256 12.240000000000002 L 384 384 L -384 384 L -384 12.240000000000002 Z" fill="#1d5167" stroke="#1d5167" strokeWidth="1" transform="translate(0 204.8)">
+                <animateTransform attributeName="transform" type="translate" values="0 204.8;256 204.8;0 204.8" keyTimes="0;0.5;1" dur="6.666666666666666s" repeatCount="indefinite" calcMode="spline" begin="0s" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" />
+              </path>
+            </g>
+          </pattern>
+        </defs>
+        <rect x="0" y="0" width="100%" height="100%" fill="url(#pid-0.09336838170898965)" />
+      </svg>
+      {children}
+    </>
+  )
+}
+
+export default Loading
